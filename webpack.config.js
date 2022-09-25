73730323ea8f5f721e612a92581bea14f396bbc1
Dylan <dylan.cobham@yahoo.com.au>
2022-09-25 20:31:25 +0930

    build: initial setup

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  mode: "development",
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    open: true,
+    compress: true,
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin ({
+        title: "To-Do List",
+        filename: "index.html",
+        template: "src/template.html"
+        })
+    ]
+  
+};
